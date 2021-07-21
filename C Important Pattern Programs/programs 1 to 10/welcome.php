5137badea81f7daf4c58fbc66b1151a6b9f1bd1a
Vivek-prog3 <85005185+Vivek-prog3@users.noreply.github.com>
2021-07-21 10:23:35 +0530

    App

--- C Important Pattern Programs/programs 1 to 10/welcome.php
+++ C Important Pattern Programs/programs 1 to 10/welcome.php
@@ -6,7 +6,7 @@
         <title>The Ultimate Coders</title>
         <link rel="stylesheet" type="text/css" href="sty.css">
         <link href="https://fonts.googleapis.com/css2?family=Audiowide&display=swap" rel="stylesheet">
-        <meta name="viewport" content="width=device-width, initial-scale=1>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
     </head>
     <body>
        <header>
